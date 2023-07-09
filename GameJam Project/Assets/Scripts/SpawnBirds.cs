3d25fdae2fca0bfb1b943765ad5770dac3457b80
KshitigSeth <kshitigvseth@gmail.com>
2023-07-09 18:41:29 +0530

    minor edits

--- GameJam Project/Assets/Scripts/SpawnBirds.cs
+++ GameJam Project/Assets/Scripts/SpawnBirds.cs
@@ -46,7 +46,7 @@ public class SpawnBirds : MonoBehaviour
 
         if (dice == 1 && Time.timeScale == 1)
         {
-            float ySpawn = Random.Range(-3.5f, 4.0f);
+            float ySpawn = Random.Range(-3.0f, 3.5f);
             birdNumber = Random.Range(1, 6);
 
             if (birdNumber == 1)
